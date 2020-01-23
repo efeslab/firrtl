4aff110309985b66dab42a280e08be30f9f4c114
Kevin Loughlin <kevlough@umich.edu>
2020-01-23 12:42:20 -0500

    Adds add liveness shadow variables

---
+++ src/main/scala/firrtl/transforms/AddLivenessShadowVariables.scala
@@ -0,0 +1,45 @@
+package firrtl.transforms
+package ProductProgram
+
+import firrtl._
+import firrtl.ir._
+
+import firrtl.Mappers._
+
+import scala.collection.mutable
+
+
+/** Adds a shadow liveness variable to each variable in a circuit, according
+  * to Iodine's definition.
+  * Should be used before ProductProgram
+  */
+class AddLivenessShadowVariables extends Transform {
+  def inputForm: CircuitForm = LowForm
+  def outputForm: CircuitForm = LowForm
+
+  def walkModule(m: DefModule): DefModule = {
+    val updated = m.map(walkPort(_))
+    updated.map(walkStatement(_))
+  }
+
+  def walkPort(p: Port): Port = {
+    p
+  }
+
+  def walkStatement(s: Statement): Statement = {
+    val block = mutable.ArrayBuffer[Statement]()
+    val 
+    val updated = s.map(walkStatement(_))
+    updated.map(walkExpression(_))
+  }
+
+  def walkExpression(e: Expression): Expression = {
+    e
+  }
+
+  def execute(state: CircuitState): CircuitState = {
+    val c = state.circuit
+    val newCircuit = c.copy(modules = c.modules.map(walkModule))
+    state.copy(circuit = newCircuit)
+  }
+}
